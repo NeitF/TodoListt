6cc68a15bff726ea1686fa191bfd794c3d31776c
Nathan Fonseca <nathan.silva@becomex.com.br>
2024-01-06 21:25:47 -0300

    configs iniciais

---
+++ TodoList.API/TodoList.Core/Context/TodoListContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Core.Entities;
+
+namespace TodoList.Core.Context
+{
+	internal class TodoListContext : DbContext
+	{
+		public TodoListContext(DbContextOptions<TodoListContext> options) : base(options) { }
+
+		public DbSet<TodoItem> Items { get; set; }
+	}
+}
---
+++ TodoList.API/TodoList.Core/Entities/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Core.Entities
+{
+	public class TodoItem
+	{
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool isCompleto { get; set; }
+
+        public TodoItem(string descricao, bool IsCompleto = false)
+        {
+            Descricao = descricao;
+            isCompleto = IsCompleto;
+        }
+    }
+}
