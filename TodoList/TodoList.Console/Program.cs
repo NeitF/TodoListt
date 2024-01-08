2e920810251041f70e2c20c23359a31013010416
Nathan Fonseca <nafs2002@hotmail.com>
2024-01-07 22:37:13 -0300

    Criando menu de opções

--- TodoList/TodoList.Console/Program.cs
+++ TodoList/TodoList.Console/Program.cs
@@ -2,17 +2,49 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoList.Core.Context;
+using TodoList.Core.Enum;
 
-var config = new ConfigurationBuilder()
-    .AddJsonFile($"appsettings.Development.json")
-    .Build();
+public class ConsoleProgram
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
 
-var services = new ServiceCollection();
-var ConnectionString = config.GetConnectionString("ConexaoPadrao");
-var MySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 35));
+        var services = new ServiceCollection();
+        var ConnectionString = config.GetConnectionString("ConexaoPadrao");
+        var MySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 35));
 
-services.AddDbContext<TodoListContext>(options => options.UseMySql(ConnectionString, MySqlServerVersion));
+        services.AddDbContext<TodoListContext>(options => options.UseMySql(ConnectionString, MySqlServerVersion));
 
-var serviceProvider = services.BuildServiceProvider(); 
-var context = serviceProvider.GetService<TodoListContext>();
+        var serviceProvider = services.BuildServiceProvider(); 
+        var context = serviceProvider.GetService<TodoListContext>();
 
+        Console.WriteLine($"Sistema operacional: {{RuntimeInformation.OSDescription}}");
+        Console.WriteLine("Lista de tarefas a fazer\n");
+
+        while (true)
+        {
+            ExibirMenu();
+            Enum.TryParse(Console.ReadLine(), true, out OpcaoMenuEnum opcao);
+        }
+        
+        
+    }         
+    internal static void ExibirMenu()
+    {
+        Console.WriteLine(new string('=', 30));
+
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1 - Adicionar tarefa\n" +
+                            "2 - Remover tarefa\n" +
+                            "3 - Editar tarefa\n" +
+                            "4 - Marcar tarefa como completa\n" +
+                            "5 - Ver todas as tarefas\n" +
+                            "6 - Buscar tarefa por Id\n" +
+                            "7 - Sair");
+
+        Console.WriteLine(new string('=', 30));
+    }
+}
---
+++ TodoList/TodoList.Core/Enum/OpcaoMenuEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Core.Enum
+{
+    public enum OpcaoMenuEnum : byte
+    {
+        Add = 1,
+        Remover,
+        Alterar,
+        MarcarComoCompletado,
+        VerTodos,
+        VerUm,
+        Sair
+    }
+}
