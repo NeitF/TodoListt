b8dcc953412e74097b713ca43ca86854d696232a
Nathan Fonseca <nafs2002@hotmail.com>
2024-01-07 21:50:55 -0300

    Criando estrutura inicial da camada Console

--- TodoList/TodoList.Console/Program.cs
+++ TodoList/TodoList.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TodoList.Core.Context;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.Development.json")
+    .Build();
+
+var services = new ServiceCollection();
+var ConnectionString = config.GetConnectionString("ConexaoPadrao");
+var MySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 35));
+
+services.AddDbContext<TodoListContext>(options => options.UseMySql(ConnectionString, MySqlServerVersion));
+
+var serviceProvider = services.BuildServiceProvider(); 
+var context = serviceProvider.GetService<TodoListContext>();
+
